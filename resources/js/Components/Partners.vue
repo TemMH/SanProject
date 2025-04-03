7c9f87fbf297ef4e4a22d2a42771747965fb8298
Artemiy Levkevich <a_gakh1@mail.ru>
2025-04-03 21:21:12 +0800

    add 20 logo partners

--- resources/js/Components/Partners.vue
+++ resources/js/Components/Partners.vue
@@ -1,9 +1,9 @@
 <template>
-    <div
-        class=" mt-48 w-full flex flex-row items-center overflow-hidden">
-
-        <div class="flex flex-row items-center justify-start  grow-0 shrink-0" id="ticker-track">
-            
+    <section class="mt-48 w-full flex flex-row items-center overflow-hidden">
+        <div
+            class="flex flex-row items-center justify-start grow-0 shrink-0"
+            id="ticker-track"
+        >
             <div
                 v-for="(logo, index) in visibleLogos"
                 :key="index"
@@ -12,18 +12,18 @@
                 <a :href="logo.url" target="_blank" rel="noopener noreferrer">
                     <img
                         :alt="logo.alt"
-                        class="rounded-lg shadow-lg"
                         height="100"
                         :src="logo.src"
                         width="150"
                     />
                 </a>
             </div>
-
         </div>
 
-        <div class="flex flex-row items-center justify-start  grow-0 shrink-0" id="ticker-track">
-            
+        <div
+            class="flex flex-row items-center justify-start grow-0 shrink-0"
+            id="ticker-track"
+        >
             <div
                 v-for="(logo, index) in visibleLogos"
                 :key="index"
@@ -32,61 +32,143 @@
                 <a :href="logo.url" target="_blank" rel="noopener noreferrer">
                     <img
                         :alt="logo.alt"
-                        class="rounded-lg shadow-lg"
                         height="100"
                         :src="logo.src"
                         width="150"
                     />
                 </a>
             </div>
-
         </div>
-
-
-    </div>
-
+    </section>
 </template>
 
 <script>
+import Logo1 from "@/Assets/Partners/aquatek.webp";
+import Logo2 from "@/Assets/Partners/aquanet.svg";
+import Logo3 from "@/Assets/Partners/aquaton.svg";
+import Logo4 from "@/Assets/Partners/aqwella.png";
+import Logo5 from "@/Assets/Partners/alcaplast.png";
+import Logo6 from "@/Assets/Partners/azario.png";
+import Logo7 from "@/Assets/Partners/am_pm.svg";
+import Logo8 from "@/Assets/Partners/sanriks.svg";
+import Logo9 from "@/Assets/Partners/ceramicanova.png";
+import Logo10 from "@/Assets/Partners/ceruttispa.png";
+import Logo11 from "@/Assets/Partners/laufen.png";
+import Logo12 from "@/Assets/Partners/roca.svg";
+import Logo13 from "@/Assets/Partners/ravak.png";
+import Logo14 from "@/Assets/Partners/grohe.png";
+import Logo15 from "@/Assets/Partners/hansgrohe.svg";
+import Logo16 from "@/Assets/Partners/belbagno.png";
+import Logo17 from "@/Assets/Partners/bettoserb.svg";
+import Logo18 from "@/Assets/Partners/caprigo.svg";
+import Logo19 from "@/Assets/Partners/cersanit.png";
+import Logo20 from "@/Assets/Partners/cezares.png";
+
 export default {
     data() {
         return {
             logos: [
                 {
-                    src: "https://placehold.co/150x100",
+                    src: Logo1,
                     alt: "Partner Logo 1",
-                    url: "https://example.com",
+                    url: "https://www.aquatek-rf.ru",
                 },
                 {
-                    src: "https://placehold.co/150x100",
+                    src: Logo2,
                     alt: "Partner Logo 2",
-                    url: "https://example.com",
+                    url: "https://www.aquanet.ru",
                 },
                 {
-                    src: "https://placehold.co/150x100",
+                    src: Logo3,
                     alt: "Partner Logo 3",
-                    url: "https://example.com",
+                    url: "https://aquaton.ru",
                 },
                 {
-                    src: "https://placehold.co/150x100",
+                    src: Logo4,
                     alt: "Partner Logo 4",
-                    url: "https://example.com",
+                    url: "https://www.aqwella.com",
                 },
                 {
-                    src: "https://placehold.co/150x100",
+                    src: Logo5,
                     alt: "Partner Logo 5",
-                    url: "https://example.com",
+                    url: "https://alcaplast.shop",
                 },
                 {
-                    src: "https://placehold.co/150x100",
+                    src: Logo6,
                     alt: "Partner Logo 6",
-                    url: "https://example.com",
+                    url: "https://azario.ru",
+                },
+                {
+                    src: Logo7,
+                    alt: "Partner Logo 7",
+                    url: "https://ampm-store.ru",
+                },
+                {
+                    src: Logo8,
+                    alt: "Partner Logo 8",
+                    url: "https://sanriks.ru",
+                },
+                {
+                    src: Logo9,
+                    alt: "Partner Logo 9",
+                    url: "https://ceramicanova.com",
+                },
+                {
+                    src: Logo10,
+                    alt: "Partner Logo 10",
+                    url: "https://www.ceruttispa-shop.ru",
+                },
+                {
+                    src: Logo11,
+                    alt: "Partner Logo 11",
+                    url: "https://www.laufen.com",
+                },
+                {
+                    src: Logo12,
+                    alt: "Partner Logo 12",
+                    url: "https://www.roca.ru",
+                },
+                {
+                    src: Logo13,
+                    alt: "Partner Logo 13",
+                    url: "https://www.ravak.ru",
+                },
+                {
+                    src: Logo14,
+                    alt: "Partner Logo 14",
+                    url: "https://grohe-russia.shop",
+                }, //Для мониторов 2к 14 логотипов 150px хватает
+                {
+                    src: Logo15,
+                    alt: "Partner Logo 15",
+                    url: "https://www.hansgrohe.ru",
+                },
+                {
+                    src: Logo16,
+                    alt: "Partner Logo 16",
+                    url: "https://belbagno.ru",
+                },
+                {
+                    src: Logo17,
+                    alt: "Partner Logo 17",
+                    url: "https://bettoserb.com",
+                },
+                {
+                    src: Logo18,
+                    alt: "Partner Logo 18",
+                    url: "https://www.caprigo.ru",
+                },
+                {
+                    src: Logo19,
+                    alt: "Partner Logo 19",
+                    url: "https://cersanit.ru",
+                },
+                {
+                    src: Logo20,
+                    alt: "Partner Logo 20",
+                    url: "https://cezares.ru",
                 },
             ],
-            offset: 0,
-            speed: 1,
-            containerWidth: 0,
-            logosWidth: 0,
         };
     },
     computed: {
@@ -94,21 +176,21 @@ export default {
             return [...this.logos];
         },
     },
-
 };
 </script>
 
 <style>
 @keyframes ticker {
-  0% {
-    transform: translateX(0%);
-  }
-  100% {
-    transform: translateX(-100%);
-  }
+    0% {
+        transform: translateX(0%);
+    }
+    100% {
+        transform: translateX(-100%);
+    }
 }
 
 #ticker-track {
-  animation: ticker 30s linear infinite;
+    animation: ticker 30s linear infinite;
 }
 </style>
+<!-- ещё Iddis, Jacob Delafon, Lemark, Paini, Ulgran, Vitra, Vincea, Grossman, Fabia, Ростовская Мануфактура Сантехника, Iddis, Kaiser, ODA ODA, RGW, Maxonor, ДВИН, Melana, Boheme -->
